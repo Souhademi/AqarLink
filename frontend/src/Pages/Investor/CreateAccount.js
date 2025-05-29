472a813905445c644952960dc3b99a0479c6d39f
souha <souhaademikha@gmail.com>
2025-05-29 18:03:10 +0100

    last fix of footer

--- frontend/src/Pages/Investor/CreateAccount.js
+++ frontend/src/Pages/Investor/CreateAccount.js
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 import Row2 from "../Row2"; // Import Row2 component
 import AutoChatBot from "../AutoChatBot";
 import axios from "axios";
-import Footer from "../Footer";  
+// import Footer from "../Footer";  
 
 const CreateInvestorAccount = () => {
-  const footerRef = useRef(null);
+  // const footerRef = useRef(null);
 
-  const handleScrollToFooter = () => {
-    footerRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
+  // const handleScrollToFooter = () => {
+  //   footerRef.current?.scrollIntoView({ behavior: 'smooth' });
+  // };
 
  const [formData, setFormData] = useState({
  
@@ -264,10 +264,10 @@ if (!passwordRegex.test(password)) {
 
         <AutoChatBot/>
       </div>     
-   
+{/*    
    <div ref={footerRef}>
         <Footer />
-      </div>
+      </div> */}
     </>
   );
 };
